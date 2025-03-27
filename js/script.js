ab568abb60a2be3d86e600ed55ceaf769d2c7584
Ivanaricci <ivana.ricci26@gmail.com>
2025-03-27 08:48:00 +0100

    [FIX] Add on e off

--- js/script.js
+++ js/script.js
@@ -5,5 +5,14 @@ const button = document.getElementById('on-off')
 
 
 button.addEventListener('click', function(){
-    image.src="./img/yellow_lamp.png"
+
+    if(image.src.includes('yellow')){
+        image.src='./img/white_lamp.png'
+        button.innerText='Accendi'
+    }
+    else{
+        image.src='./img/yellow_lamp.png'
+        button.innerText='Spegni'
+    }
+    // image.src="./img/yellow_lamp.png"
 })
